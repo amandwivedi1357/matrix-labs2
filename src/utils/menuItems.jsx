1e23c7a3867552132414099cae5b33b3dc7a6d77
Aman Dwivedi <amandwivedi71124.com>
2023-09-08 18:32:02 +0530

    first commit

---
+++ src/utils/menuItems.jsx
@@ -0,0 +1,17 @@
+import {dashboard, transactions} from './Icons'
+
+export const menuItems = [
+    {
+        id: 1,
+        title: 'token address',
+        icon: dashboard,
+        link: '/token-address'
+    },
+    {
+        id: 2,
+        title: "pair address",
+        icon: transactions,
+        link: "/pair-address",
+    },
+   
+]
